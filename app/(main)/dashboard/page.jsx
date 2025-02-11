5deaf6d613ee04ab6b9bfa305ed687118e063f08
Vipulpandey007 <vipulpandey708@gmail.com>
2025-02-11 22:11:00 +0530

    monthly budget added to dashboard page

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,36 +1,35 @@
 import { Suspense } from "react";
 import { getUserAccounts } from "@/actions/dashboard";
-// import { getDashboardData } from "@/actions/dashboard";
-// import { getCurrentBudget } from "@/actions/budget";
-
-// import { BudgetProgress } from "./_components/budget-progress";
+import { getDashboardData } from "@/actions/dashboard";
+import { getCurrentBudget } from "@/actions/budget";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 import { CreateAccountDrawer } from "@/components/CreateAccountDrawer";
 import { AccountCard } from "./_components/AccountCard";
+import { BudgetProgress } from "./_components/BudgetProgress";
 // import { DashboardOverview } from "./_components/transaction-overview";
 
 export default async function DashboardPage() {
   const [accounts, transactions] = await Promise.all([
     getUserAccounts(),
-    // getDashboardData(),
+    getDashboardData(),
   ]);
 
-  // const defaultAccount = accounts?.find((account) => account.isDefault);
+  const defaultAccount = accounts?.find((account) => account.isDefault);
 
   // Get budget for default account
   let budgetData = null;
-  // if (defaultAccount) {
-  //   budgetData = await getCurrentBudget(defaultAccount.id);
-  // }
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
 
   return (
     <div className="space-y-8">
       {/* Budget Progress */}
-      {/* <BudgetProgress
+      <BudgetProgress
         initialBudget={budgetData?.budget}
         currentExpenses={budgetData?.currentExpenses || 0}
-      /> */}
+      />
 
       {/* Dashboard Overview */}
       {/* <DashboardOverview
