56239cd51b77e6d9419c56ca088df9d36fdf2d6f
Vipulpandey007 <vipulpandey708@gmail.com>
2025-02-27 12:43:04 +0530

    link fixed

--- components/Header.jsx
+++ components/Header.jsx
@@ -49,12 +49,12 @@ const Header = async () => {
                 <span className="hidden md:inline">Dashboard</span>
               </Button>
             </Link>
-            <a href="/transaction/create">
+            <Link href="/transaction/create">
               <Button className="flex items-center gap-2">
                 <PenBox size={18} />
                 <span className="hidden md:inline">Add Transaction</span>
               </Button>
-            </a>
+            </Link>
           </SignedIn>
           <SignedOut>
             <SignInButton forceRedirectUrl="/dashboard">
